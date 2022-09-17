2ca276da5aba7d955b7a8fdd1f332dd841a699d8
Md Ashikur Rahman <50617850+ashikur7748@users.noreply.github.com>
2022-09-17 20:42:27 +0600

    file create

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import CutomerLogin from './CutomerLogin';
+
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <CutomerLogin/>
+    );
+  }
+}
+
+export default App;
+
