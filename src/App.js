d84a51689931ac743ce95922ffdaad34bc4b6845
unknown <rakib35@gmail.com>
2022-09-24 00:23:18 +0600

    updates

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 // import React, { Component } from 'react';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+//  
 
 
-// import './modules/core/css/style.css';
+// 
 
 // class App extends Component {
 //     render() {
