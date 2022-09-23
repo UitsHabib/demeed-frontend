5c72c1c5c3846408cc90144b471ba9cd5b2d3ce9
kzjahad <kzjahad@gmail.com>
2022-09-24 01:17:10 +0600

    Add Permission Table

--- src/modules/admin/components/permission-table.component.js
+++ src/modules/admin/components/permission-table.component.js
@@ -4,7 +4,63 @@ class PermissionTable extends Component {
     state = {  } 
     render() { 
         return (
-            <h1>Permission Table</h1>
+            <div className='container'>
+                <h1 className='mt-3'>Permission Table</h1>
+                <div className='row'>
+                    <div className='col-8'>
+                        <table className='table table-bordered table-hover'>
+                            <thead className='table-dark'>
+                                <tr>
+                                <th scope="col">ACTION</th>
+                                <th scope="col">PERMISSION</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Lorem Ipsum</td>
+                                    <td>
+                                        <div class="form-check form-switch">
+                                            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Lorem Ipsum</td>
+                                    <td>
+                                        <div class="form-check form-switch">
+                                            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Lorem Ipsum</td>
+                                    <td>
+                                        <div class="form-check form-switch">
+                                            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Lorem Ipsum</td>
+                                    <td>
+                                        <div class="form-check form-switch">
+                                            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td col>Lorem Ipsum</td>
+                                    <td>
+                                        <div class="form-check form-switch">
+                                            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+                                        </div>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
